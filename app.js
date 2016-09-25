af764b82bc9bae384451cb7405aca78364e53c72
ozyx <jessemazzella@gmail.com>
2016-09-25 14:17:16 -0700

    add bear

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-const {remote} = require('electron')
-const {Menu, MenuItem} = remote
+/* jslint es6 */
+const remote = require('electron').remote;
+const {Menu, MenuItem} = remote;
+const main = remote.require('./index.js')
 
-const menu = new Menu()
-menu.append(new MenuItem({label: 'MenuItem1', click() { alert('item 1 clicked') }}))
-menu.append(new MenuItem({type: 'separator'}))
-menu.append(new MenuItem({label: 'MenuItem2', type: 'checkbox', checked: true}))
+const menu = new Menu();
+menu.append(new MenuItem({label: 'MenuItem1', click() { alert('item 1 clicked') }}));
+menu.append(new MenuItem({type: 'separator'}));
+menu.append(new MenuItem({label: 'MenuItem2', type: 'checkbox', checked: true}));
 
 window.addEventListener('contextmenu', (e) => {
   e.preventDefault()
   menu.popup(remote.getCurrentWindow())
+}, false);
+
+let button = document.createElement('button')
+button.textContent = 'Wanna see a bear?'
+button.addEventListener('click', () => {
+    main.openWindow('bear.html')
 }, false)
+document.body.appendChild(button)
+
--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 /* jslint es6 */
-const {app} = require('electron');
-const {BrowserWindow} = require('electron');
+'use strict'
+const electron = require('electron')
+const {app, BrowserWindow} = electron
 
-app.on('ready', function (){
-    var mainWindow = new BrowserWindow({
+app.on('ready', () => {
+    let mainWindow = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        backgroundColor: '#2e2c29',
+        resizable: false
     });
-    mainWindow.loadURL('file://' + __dirname + '/index.html');
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
+
+exports.openWindow = (page) => {
+    let win = new BrowserWindow({
+        width: 600,
+        height: 600,
+        backgroundColor: '#2e2c29',
+        resizable: false
+    })
+    win.loadURL(`file://${__dirname}/${page}`)
+}
