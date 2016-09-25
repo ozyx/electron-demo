d926b25a908a5d82dcd93364891fa4a270c65aec
ozyx <jessemazzella@gmail.com>
2016-09-25 16:56:12 -0700

    add tray icon

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
 /* jslint es6 */
 'use strict'
 const electron = require('electron')
-const {app, BrowserWindow} = electron
-
+const {app, BrowserWindow, Tray, Menu} = electron
+let tray = null;
 app.on('ready', () => {
+    tray = new Tray('res/bear.ico');
+    const contextMenu = Menu.buildFromTemplate([
+        { label: 'Item1', type: 'radio' },
+        { label: 'Item2', type: 'radio' },
+        { label: 'Item3', type: 'radio', checked: true },
+        { label: 'Item4', type: 'radio' }
+    ])
+    tray.setToolTip('Bears & Dice')
+    tray.setContextMenu(contextMenu)
+
     let mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
         backgroundColor: '#2e2c29',
-        resizable: false
+        resizable: false,
+        devTools: true
     });
+    // mainWindow.setMenu(null)
     mainWindow.loadURL(`file://${__dirname}/main.html`);
 
-    electron.powerMonitor.on('on-ac', ()=>{
+    electron.powerMonitor.on('on-ac', () => {
         console.log('power plugged in')
     })
 });
