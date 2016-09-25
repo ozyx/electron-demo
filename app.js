b344751ce274763d3de7ef99a29245619b67a4f8
ozyx <jessemazzella@gmail.com>
2016-09-25 16:56:23 -0700

    fix button styles

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 /* jslint es6 */
-const remote = require('electron').remote;
-const {Menu, MenuItem} = remote;
-const main = remote.require('./main.js')
+let remote = require('electron').remote;
+let {Menu, MenuItem} = remote;
+let main = remote.require('./main.js')
 
-const menu = new Menu();
+function createButton(text, func){
+    let button = document.createElement('button')
+    button.setAttribute("class", "button")
+    button.textContent = text
+    button.addEventListener('click', func, false)
+    document.body.appendChild(button)
+}
+
+let menu = new Menu();
 menu.append(new MenuItem({label: 'MenuItem1', click() { alert('item 1 clicked') }}));
 menu.append(new MenuItem({type: 'separator'}));
 menu.append(new MenuItem({label: 'MenuItem2', type: 'checkbox', checked: true}));
@@ -13,20 +21,9 @@ window.addEventListener('contextmenu', (e) => {
   menu.popup(remote.getCurrentWindow())
 }, false);
 
-let button = document.createElement('button')
-button.textContent = 'Wanna see a bear?'
-button.addEventListener('click', () => {
-    main.openWindow('bear.html')
-}, false)
-document.body.appendChild(button)
-
-button = document.createElement('button')
-button.textContent = 'roll some dice?'
-button.addEventListener('click', () => {
+createButton('Wanna see a bear?', () => {main.openWindow('bear.html')})
+createButton('Roll some dice?', () => {
     let dice3d = require('dice3d')
     let result = Math.floor(Math.random() * 7)
-    dice3d(6, result, () => {
-        console.log('hey')
-    })
-}, false)
-document.body.appendChild(button)
+    dice3d(6, result, () => {})
+})
